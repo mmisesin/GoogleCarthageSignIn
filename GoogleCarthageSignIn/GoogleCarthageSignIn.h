a747662276a0be571a83a71c57562f6ccb48b09d
Artem Misesin <misesin@Artems-MacBook-Pro.local>
2017-08-22 11:54:29 +0300

    Initial Commit

---
+++ GoogleCarthageSignIn/GoogleCarthageSignIn.h
@@ -0,0 +1,19 @@
+//
+//  GoogleCarthageSignIn.h
+//  GoogleCarthageSignIn
+//
+//  Created by Artem Misesin on 8/22/17.
+//  Copyright © 2017 Artem Misesin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GoogleCarthageSignIn.
+FOUNDATION_EXPORT double GoogleCarthageSignInVersionNumber;
+
+//! Project version string for GoogleCarthageSignIn.
+FOUNDATION_EXPORT const unsigned char GoogleCarthageSignInVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GoogleCarthageSignIn/PublicHeader.h>
+
+
